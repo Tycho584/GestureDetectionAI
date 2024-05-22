21e080dfb44ba467cc7d0d5ab6eb598513611855
tycho584 <tycho.killaars@outlook.com>
2024-05-22 10:00:36 +0200

    working model with pictures, not live stream

--- googleSearchTest.py
+++ googleSearchTest.py
@@ -1,33 +1,128 @@
+#@markdown We implemented some functions to visualize the gesture recognition results. <br/> Run the following cell to activate the functions.
+from matplotlib import pyplot as plt
 import mediapipe as mp
+from mediapipe.framework.formats import landmark_pb2
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import urllib
 import cv2
+import math
 
-mp_drawing = mp.solutions.drawing_utils
+DESIRED_HEIGHT = 480
+DESIRED_WIDTH = 480
+
+plt.rcParams.update({
+    'axes.spines.top': False,
+    'axes.spines.right': False,
+    'axes.spines.left': False,
+    'axes.spines.bottom': False,
+    'xtick.labelbottom': False,
+    'xtick.bottom': False,
+    'ytick.labelleft': False,
+    'ytick.left': False,
+    'xtick.labeltop': False,
+    'xtick.top': False,
+    'ytick.labelright': False,
+    'ytick.right': False
+})
+
 mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 
-capture = cv2.VideoCapture(0)
 
-with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
-  while capture.isOpened():
-      ret, frame = capture.read()
-      frame = cv2.flip(frame, 1)
-      image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-      detected_image = hands.process(image)
-      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-  
-      if detected_image.multi_hand_landmarks:
-          for hand_lms in detected_image.multi_hand_landmarks:
-              mp_drawing.draw_landmarks(image, hand_lms,
-                                        mp_hands.HAND_CONNECTIONS,
-                                        landmark_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(
-                                            color=(255, 0, 255), thickness=4, circle_radius=2),
-                                        connection_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(
-                                            color=(20, 180, 90), thickness=2, circle_radius=2)
-                                        )
-  
-      cv2.imshow('Webcam', image)
-  
-      if cv2.waitKey(1) & 0xFF == ord('q'):
-          break
+def display_one_image(image, title, subplot, titlesize=16):
+    """Displays one image along with the predicted category name and score."""
+    plt.subplot(*subplot)
+    plt.imshow(image)
+    if len(title) > 0:
+        plt.title(title, fontsize=int(titlesize), color='black', fontdict={'verticalalignment':'center'}, pad=int(titlesize/1.5))
+    return (subplot[0], subplot[1], subplot[2]+1)
 
-capture.release()
-cv2.destroyAllWindows()
+
+def display_batch_of_images_with_gestures_and_hand_landmarks(images, results):
+    """Displays a batch of images with the gesture category and its score along with the hand landmarks."""
+    # Images and labels.
+    images = [image.numpy_view() for image in images]
+    gestures = [top_gesture for (top_gesture, _) in results]
+    multi_hand_landmarks_list = [multi_hand_landmarks for (_, multi_hand_landmarks) in results]
+
+    # Auto-squaring: this will drop data that does not fit into square or square-ish rectangle.
+    rows = int(math.sqrt(len(images)))
+    cols = len(images) // rows
+
+    # Size and spacing.
+    FIGSIZE = 13.0
+    SPACING = 0.1
+    subplot=(rows,cols, 1)
+    if rows < cols:
+        plt.figure(figsize=(FIGSIZE,FIGSIZE/cols*rows))
+    else:
+        plt.figure(figsize=(FIGSIZE/rows*cols,FIGSIZE))
+
+    # Display gestures and hand landmarks.
+    for i, (image, gestures) in enumerate(zip(images[:rows*cols], gestures[:rows*cols])):
+        title = f"{gestures.category_name} ({gestures.score:.2f})"
+        dynamic_titlesize = FIGSIZE*SPACING/max(rows,cols) * 40 + 3
+        annotated_image = image.copy()
+
+        for hand_landmarks in multi_hand_landmarks_list[i]:
+            hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            hand_landmarks_proto.landmark.extend([
+                landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+            ])
+
+            mp_drawing.draw_landmarks(
+                annotated_image,
+                hand_landmarks_proto,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+        subplot = display_one_image(annotated_image, title, subplot, titlesize=dynamic_titlesize)
+
+    # Layout.
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=SPACING, hspace=SPACING)
+    plt.show()
+
+def resize_and_show(image):
+    h, w = image.shape[:2]
+    if h < w:
+        img = cv2.resize(image, (DESIRED_WIDTH, math.floor(h/(w/DESIRED_WIDTH))))
+    else:
+        img = cv2.resize(image, (math.floor(w/(h/DESIRED_HEIGHT)), DESIRED_HEIGHT))
+    # cv2.imshow(img)
+
+
+IMAGE_FILENAMES = ['thumbs_down.jpg', 'victory.jpg', 'thumbs_up.jpg', 'pointing_up.jpg']
+
+for name in IMAGE_FILENAMES:
+    url = f'https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/{name}'
+    urllib.request.urlretrieve(url, name)
+
+images = {name: cv2.imread(name) for name in IMAGE_FILENAMES}
+for name, image in images.items():
+    print(name)
+    resize_and_show(image)
+
+base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
+options = vision.GestureRecognizerOptions(base_options=base_options)
+recognizer = vision.GestureRecognizer.create_from_options(options)
+
+images = []
+results = []
+for image_file_name in IMAGE_FILENAMES:
+    # STEP 3: Load the input image.
+    image = mp.Image.create_from_file(image_file_name)
+
+    # STEP 4: Recognize gestures in the input image.
+    recognition_result = recognizer.recognize(image)
+
+    # STEP 5: Process the result. In this case, visualize it.
+    images.append(image)
+    top_gesture = recognition_result.gestures[0][0]
+    hand_landmarks = recognition_result.hand_landmarks
+    results.append((top_gesture, hand_landmarks))
+
+display_batch_of_images_with_gestures_and_hand_landmarks(images, results)
